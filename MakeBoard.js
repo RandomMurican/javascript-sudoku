1022d0c20526a28991430d6431f918687124d1cd
RandomMurican <randommurican@gmail.com>
2021-05-24 16:15:20 -0500

    only one cell can be selected

--- MakeBoard.js
+++ MakeBoard.js
@@ -5,7 +5,7 @@ window.onload = function init() {
         row.classList.add('row');
         for (let i = 1; i < 10; i++) {
             let cell = document.createElement('td');
-            cell.id = 'cell-' + (i + x * 9);
+            cell.id = 'cell-' + (i + x * 9 - 1);
             cell.setAttribute("onclick","selectBox('" + cell.id + "')");
             cell.classList.add('cell');
             if (i % 3 == 0) {
@@ -14,7 +14,7 @@ window.onload = function init() {
             if (x % 3 == 0) {
                 cell.classList.add('border-t')
             }
-            cell.innerText = i;
+            cell.innerText = i + x * 9 - 1;
             row.appendChild(cell);
         }
         board.appendChild(row);
@@ -22,6 +22,13 @@ window.onload = function init() {
 }
 
 function selectBox(id) {
-    element = document.getElementById(id);
-    element.classList.toggle("selected");
+    let cell = document.getElementById(id);
+    if (cell) {
+        for (let i = 0; i < 81; i++) {
+            if ('cell-'+i === id)
+                continue
+            document.getElementById('cell-'+i).classList.remove('selected');
+        }
+        cell.classList.toggle("selected")
+    }
 }
